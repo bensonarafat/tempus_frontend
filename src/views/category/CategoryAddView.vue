0939f11e31bec55362a028ce38c15395286cd86e
Benson Arafat <benpaul320@gmail.com>
2024-12-13 00:45:19 +0100

    Update dashboard

--- src/views/category/CategoryAddView.vue
+++ src/views/category/CategoryAddView.vue
@@ -4,48 +4,20 @@ import { useCategoryStore } from '@/stores/modules/category'
 import { type CategoryDto } from '@/stores/interfaces/category.interface'
 import SuccessAlert from '@/components/SuccessAlert.vue'
 import ErrorAlert from '@/components/ErrorAlert.vue'
+import ImageUploader from '@/components/ImageUploader.vue'
 
 const categoryStore = useCategoryStore()
+const imageUploaderRef = ref<InstanceType<typeof ImageUploader> | null>(null)
 
 const loading = ref(false)
-const imagePreview = ref('')
 const error = ref<null | string>('')
 const success = ref('')
 const imageFile = ref(null as File | null)
-const imageUrl = ref('')
 const name = ref('')
 const description = ref('')
 
-const removeImage = () => {
-  imageFile.value = null
-  imagePreview.value = ''
-  imageUrl.value = ''
-}
-
-const handleImageChange = (event: Event) => {
-  const input = event.target as HTMLInputElement
-  const files = input.files
-
-  if (files && files.length > 0) {
-    const file = files[0]
-
-    // Validate file type
-    if (!file.type.startsWith('image/')) {
-      error.value = 'Please upload an image file'
-      return false
-    }
-
-    // Validate file size (e.g. 5MB limit)
-    if (file.size > 5 * 1024 * 1024) {
-      error.value = 'Image must be less than 5MB'
-      return
-    }
-
-    imageFile.value = file
-
-    // Create preview URL
-    imagePreview.value = URL.createObjectURL(file)
-  }
+const handleImagePicker = (data: File) => {
+  imageFile.value = data
 }
 
 const handleSubmit = async () => {
@@ -81,8 +53,7 @@ const handleSubmit = async () => {
 const resetForm = () => {
   name.value = ''
   description.value = ''
-  // remove Images
-  removeImage()
+  imageUploaderRef.value?.removeImage()
 }
 </script>
 <template>
@@ -199,71 +170,11 @@ const resetForm = () => {
                 />
               </div>
 
-              <!-- Image Upload Section -->
-              <div class="mb-5">
-                <label class="block mb-2 text-sm font-medium dark:text-white text-gray-900"
-                  >Image</label
-                >
-                <div class="flex items-center justify-center w-full">
-                  <label
-                    for="image-upload"
-                    class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
-                  >
-                    <div
-                      v-if="!imagePreview"
-                      class="flex flex-col items-center justify-center pt-5 pb-6"
-                    >
-                      <svg
-                        class="w-8 h-8 mb-4 text-gray-500"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 20 16"
-                      >
-                        <path
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                        />
-                      </svg>
-                      <p class="mb-2 text-sm text-gray-500">
-                        <span class="font-semibold">Click to upload</span> or drag and drop
-                      </p>
-                      <p class="text-xs text-gray-500">PNG, JPG or GIF (MAX. 5MB)</p>
-                    </div>
-                    <div v-else class="relative w-full h-full">
-                      <img :src="imagePreview" class="object-cover w-full h-full rounded-lg" />
-                      <button
-                        @click.prevent="removeImage"
-                        class="absolute top-2 right-2 bg-red-500 text-white rounded-full p-1 hover:bg-red-600"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="h-5 w-5"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                    <input
-                      id="image-upload"
-                      type="file"
-                      class="hidden"
-                      accept="image/*"
-                      @change="handleImageChange"
-                      :disabled="loading"
-                    />
-                  </label>
-                </div>
-              </div>
+              <ImageUploader
+                ref="imageUploaderRef"
+                label="Feature Image"
+                @image-picked="handleImagePicker"
+              />
 
               <div class="col-span-6 sm:col-full">
                 <button
